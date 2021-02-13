9816b369fed369ab1e493a4f4424d052c501b676
MohammadR-M <mohammadrezamirzadejava@gmail.com>
2021-02-13 19:44:36 +0330

    create app.js file.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const api = require('./router/api')
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+
+app.use('/', api);
+
+
+app.listen(8080);
